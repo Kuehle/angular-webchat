ae91111407bad6dbd2ded13c100b44349778cced
Johannes Kühlewindt <johannes.kuehlewindt@bytabo.de>
2017-11-23 15:19:27 +0100

    websocket working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
+import { ChatService } from './chat.service';
+import { WebsocketService } from './websocket.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { ChatComponent } from './chat/chat.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    ChatService,
+    WebsocketService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
